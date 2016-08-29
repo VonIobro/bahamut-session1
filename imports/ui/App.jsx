a0afee994e2b4b64d724719390195df957125f24
VonIobro <jonathan.zobro@gmail.com>
2016-08-30 00:14:39 +0700

    add other George and Joe's files

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import LayoutA1 from './layouts/LayoutA1.jsx';
+import LayoutB1 from './layouts/LayoutB1.jsx';
+import LayoutC1 from './layouts/LayoutC1.jsx';
 
 export default class App extends Component {
   render() {
@@ -7,7 +9,7 @@ export default class App extends Component {
       <div className="container">
         <LayoutA1 />
         <LayoutB1 />
-        <LayoutC1 />
+        <LayoulC1 />
       </div>
     );
   }
--- imports/ui/layouts/LayoutB1.jsx
+++ imports/ui/layouts/LayoutB1.jsx
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import './LayoutA1.css';
 
 export default class LayoutA1 extends Component {
   render() {
     return (
       <div className="layout">
-        <header>header</header>
-        <body>
-          LayoutA1
-        </body>
-        <footer>footer</footer>
       </div>
     );
   }
