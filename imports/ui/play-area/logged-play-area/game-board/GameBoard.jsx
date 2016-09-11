94e76c5e7d2508a76c0f2d43759789e00c480e55
VonIobro <jonathan.zobro@gmail.com>
2016-09-12 03:02:20 +0700

    init playeravatar

--- imports/ui/play-area/logged-play-area/game-board/GameBoard.jsx
+++ imports/ui/play-area/logged-play-area/game-board/GameBoard.jsx
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {Row} from 'react-bootstrap';
 import './GameBoard.scss';
+import PlayerAvatar from './player-avatar/PlayerAvatar';
 
 export default class GameBoard extends Component {
   render() {
     return (
       <Row id="gameboard">
-        Gameboard!
+        <PlayerAvatar />
       </Row>
     );
   }
---
+++ imports/ui/play-area/logged-play-area/game-board/player-avatar/PlayerAvatar.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './PlayerAvatar.scss';
+
+export default class PlayerAvatar extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
