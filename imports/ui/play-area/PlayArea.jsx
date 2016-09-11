0e397fbaac281d42d80af55e91c519811b5d3f75
VonIobro <jonathan.zobro@gmail.com>
2016-09-11 23:06:53 +0700

    differnet areas depending on user type

--- imports/ui/play-area/PlayArea.jsx
+++ imports/ui/play-area/PlayArea.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
 import UnloggedPlayArea from './unlogged-play-area/UnloggedPlayArea';
-// import LoggedPlayArea from './logged-play-area/UnloggedPlayArea';
+import LoggedPlayArea from './logged-play-area/LoggedPlayArea';
 
 export default class PlayArea extends Component {
   render() {
     return (
       <div>
-        <UnloggedPlayArea />
+        {Meteor.user() ? <LoggedPlayArea/> : <UnloggedPlayArea />}
       </div>
     );
   }
