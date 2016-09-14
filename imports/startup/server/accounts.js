bcd54ba9e1c352972bab731de5642cb788170f62
VonIobro <jonathan.zobro@gmail.com>
2016-09-15 00:22:48 +0700

    tweak player generation rules

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -2,17 +2,20 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
 // dimensions of playboard
-const boardX = [ 0, 450 ];
-const boardY = [ 0, 600 ];
+// divided by 10 to force grid
+// user will also not be generated on edges of board
+const boardX = [ 2, 43 ];
+const boardY = [ 2, 58 ];
 const rotations = [ 0, 90, 180, 270 ];
 
 // Math.floor(Math.random() * ((y-x)+1) + x);
 // http://goo.gl/OfvsPH <- about random num gen
+// multiplied by 10 to force grid
 var randomX = () => {
-  return Math.floor(Math.random() * ((boardX[1] - boardX[0]) + 1) + boardX[0]);
+  return Math.floor(Math.random() * ((boardX[1] - boardX[0]) + 1) + boardX[0]) * 10;
 };
 var randomY = () => {
-  return Math.floor(Math.random() * ((boardY[1] - boardY[0]) + 1) + boardY[0]);
+  return Math.floor(Math.random() * ((boardY[1] - boardY[0]) + 1) + boardY[0]) * 10;
 };
 var randomRotation = () => {
   return rotations[
