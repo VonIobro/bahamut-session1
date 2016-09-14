53c5d316b4dd3a63b01fc17d6dd9bdd14aade838
VonIobro <jonathan.zobro@gmail.com>
2016-09-14 13:43:41 +0700

    Example https://facebook.github.io/react/docs/animation.html#high-level-api-reactcsstransitiongroup

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,24 +4,26 @@ import {composeWithTracker} from 'react-komposer';
 import {Col, Row} from 'react-bootstrap';
 import NavbarMain from './navbar/NavbarMain';
 import PlayArea from './play-area/PlayArea';
+import Test from './play-area/logged-play-area/game-board/player-avatar/test';
 
 class App extends Component {
   render() {
     const {user} = this.props;
     return (
       <div>
-        <Row>
-          <NavbarMain user={user}/>
-        </Row>
-        <Row>
-          <Col>
-            <PlayArea user={user}/>
-          </Col>
-        </Row>
+        <Test/>
       </div>
     );
   }
 }
+        // <Row>
+        //   <NavbarMain user={user}/>
+        // </Row>
+        // <Row>
+        //   <Col>
+        //     <PlayArea user={user}/>
+        //   </Col>
+        // </Row>
 
 export default composeWithTracker((props, onData) => {
   var user = Meteor.user();
