1ce81ef2b478bfe688b24fa8d84c00e2626461b2
VonIobro <jonathan.zobro@gmail.com>
2016-09-19 23:23:47 +0700

    test ingame notifications

--- imports/ui/play-area/logged-play-area/LoggedPlayArea.jsx
+++ imports/ui/play-area/logged-play-area/LoggedPlayArea.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import GameBoard from './game-board/GameBoard';
-import PlayerControls from './player-controls/PlayerControls';
 import DebugControls from './debug-controls/DebugControls';
+import GameBoard from './game-board/GameBoard';
+import Notify from '/imports/ui/notifications/Notify';
+import PlayerControls from './player-controls/PlayerControls';
 
 export default class LoggedPlayArea extends Component {
   render() {
     const {user} = this.props;
+    const testMessages = [
+      {
+        _id: 'sldkfjsl',
+        text: 'testing',
+        style: 'success',
+      }
+    ];
     return (
       <div className="container">
+        <Notify messages={testMessages}/>
         <GameBoard user={user}/>
         <PlayerControls user={user}/>
         <DebugControls user={user}/>
